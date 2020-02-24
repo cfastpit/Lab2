3676f3f1f3403ba6fbdda6397080394b7706e634
cfastpit <>
2020-02-23 22:01:10 -0600

    bar chart is done

--- Lab2/src/main/java/Bar_Chart.java
+++ Lab2/src/main/java/Bar_Chart.java
@@ -17,17 +17,55 @@ public class Bar_Chart {
         double store3 = 0;
         double store4 = 0;
         double store5 = 0;
+        String store1count = "";
+        String store2count = "";
+        String store3count = "";
+        String store4count = "";
+        String store5count = "";
         System.out.println("Please enter today's sales for store 1 ");
         store1 = keyboard.nextDouble();
-        System.out.println("Please enter today's sales for store 1 ");
+
+        System.out.println("Please enter today's sales for store 2 ");
         store2 = keyboard.nextDouble();
-        System.out.println("Please enter today's sales for store 1 ");
+        System.out.println("Please enter today's sales for store 3 ");
         store3 = keyboard.nextDouble();
-        System.out.println("Please enter today's sales for store 1 ");
+        System.out.println("Please enter today's sales for store 4 ");
         store4 = keyboard.nextDouble();
-        System.out.println("Please enter today's sales for store 1 ");
-       // store5 = keyboard.nextDouble();System.out.println (store1);
+        System.out.println("Please enter today's sales for store 5 ");
+        store5 = keyboard.nextDouble();
+
+        for (double i = store1; i >= 100; i = i - 100) {
+
+            store1count = store1count + "*";
+        }
+        for (double i = store2; i >= 100; i = i - 100) {
+
+            store2count = store2count + "*";
+        }
+        for (double i = store3; i >= 100; i = i - 100) {
+
+            store3count = store3count + "*";
+        }
+        for (double i = store4; i >= 100; i = i - 100) {
+
+            store4count = store4count + "*";
+        }
+        for (double i = store5; i >= 100; i = i - 100) {
+
+            store5count = store5count + "*";
+        }
+        System.out.println("SALES BAR CHART");
+        System.out.println("today's sales for store 1 is " + store1);
+        System.out.println("today's sales for store 2 is " + store2);
+        System.out.println("today's sales for store 3 is " + store3);
+        System.out.println("today's sales for store 4 is " + store4);
+        System.out.println("today's sales for store 5 is " + store5);
+        System.out.println("SALES BAR CHART");
+        System.out.println("Store 1: " + store1count);
+        System.out.println("Store 2: " + store2count);
+        System.out.println("Store 3: " + store3count);
+        System.out.println("Store 4: " + store4count);
+        System.out.println("Store 5: " + store5count);
     }
 
-    
 }
