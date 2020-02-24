40e185421d8ee64a5ca34820e4088c131fc73413
theki <theki@DESKTOP-530HBLH.attlocal.net>
2020-02-23 21:05:13 -0600

    sum of numbers are done

--- Lab2/src/main/java/Sum_Of_Numbers.java
+++ Lab2/src/main/java/Sum_Of_Numbers.java
@@ -1,7 +1,27 @@
 
+import java.util.Scanner;
+
 public class Sum_Of_Numbers {
+
     public static void main(String[] args) {
-        
+
+        /* Write a program that asks the user for a positive nonzero integer value. 
+    The program should use a loop to get the sum of all the integers 
+    from 1 up to the number entered. For example, 
+    if the user enters 50, the loop will find the sum of 1,2,3,4...50.
+         */
+        System.out.println("please Enter a positive nonzero integer value ");
+        Scanner keyboard = new Scanner(System.in);
+
+        // make an integer variable for the most recent number entered
+        int numberEntered = keyboard.nextInt();
+        int x = 0;
+
+        while (numberEntered > 0) {
+            //System.out.println("The value of x is: " + x);
+            x = x + numberEntered;
+            numberEntered=numberEntered-1;
+        }
+System.out.println("x = "+ x);
     }
-   
 }
